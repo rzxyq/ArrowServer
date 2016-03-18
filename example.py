f56df48edb1b23c001e56e090b15e406f28069ab
Ruoyan Qin <rq25@cornell.edu>
2016-03-18 17:24:18 -0400

    Changed url path

--- example.py
+++ example.py
@@ -1,6 +1,8 @@
 import json
 import requests
 
-data=json.dumps({'num':16072629999})
-url='https://arrowapplication.herokuapp.com/'
+data=json.dumps({'num':19146109496})
+url='https://arrowapplication.herokuapp.com/phoneAuth/'
+url2='http://localhost:8000/phoneAuth/'
+
 r = requests.post(url, data=data,auth=('user', 'pass'))
